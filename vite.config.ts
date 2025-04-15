844a7d8d40274b8a2ccccb6189e5ce908390a50d
devang <dtrathod777@gmail.com>
2025-04-15 12:11:03 +0530

    Build push

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/foremostsystems/',
   plugins: [react(), tailwindcss(),],
+  build: {
+    outDir: 'build',
+  }
+
 })
