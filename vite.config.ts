04a8a782753a326134bf23d33496c07e026c895d
devang <dtrathod777@gmail.com>
2025-11-17 17:26:10 +0530

    Fix And psuh

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'foremostsystems',
   plugins: [react(), tailwindcss(),],
   build: {
     outDir: 'build',
